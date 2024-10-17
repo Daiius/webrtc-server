56cdda69501eddde4e8a245bb1dfca264db62b95
Daiji Yamashita <yamashita.daiji@gmail.com>
2024-10-17 21:10:39 +0900

    videmusに合わせてパラメータ調整
    それでも再生できないので、サーバ側の問題の可能性が高くなった

--- src/client.ts
+++ src/client.ts
@@ -3,13 +3,13 @@ import { Device } from 'mediasoup-client';
 export const createStream = async () => {
   const device = new Device();
   const routerRtpCapabilitiesResponse = await fetch(
-    'http://localhost:3000/mediasoup/router-rtp-capabilities'
+    `http://localhost/videmus/api/mediasoup/router-rtp-capabilities/yellow-chart`
   );
   const routerRtpCapabilities = await routerRtpCapabilitiesResponse.json();
   console.log('routerRtpCapabilities: %o', routerRtpCapabilities);
   await device.load({ routerRtpCapabilities });
   const transportParametersResponse = await fetch(
-    'http://localhost:3000/mediasoup/streamer-transport-parameters'
+    `http://localhost/videmus/api/mediasoup/streamer-transport-parameters/yellow-chart`
   );
   const transportParameters = await transportParametersResponse.json();
   console.log('transportParmaeters: %o', transportParameters);
@@ -20,7 +20,7 @@ export const createStream = async () => {
       console.log('transport connect');
       try {
         await fetch(
-          'http://localhost:3000/mediasoup/client-connect', {
+          `http://localhost/videmus/api/mediasoup/client-connect/yellow-chart/${transportParameters.id}`, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(dtlsParameters),
@@ -36,15 +36,18 @@ export const createStream = async () => {
   });
 
   const consumerParametersResponse = await fetch(
-    'http://localhost:3000/mediasoup/consumer-parameters', {
+    `http://localhost/videmus/api/mediasoup/consumer-parameters/yellow-chart/${transportParameters.id}`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(device.rtpCapabilities),
   });
   const consumerParameters = await consumerParametersResponse.json();
-  const videoConsumer = await transport.consume(consumerParameters.video);
-  const audioConsumer = await transport.consume(consumerParameters.audio);
-  
+  const videoConsumerParameter = consumerParameters.find(p => p.kind === 'video');
+  const videoConsumer = videoConsumerParameter
+    && await transport.consume(videoConsumerParameter);
+  const audioConsumerParameter = consumerParameters.find(p => p.kind === 'audio');
+  const audioConsumer = audioConsumerParameter
+    && await transport.consume(audioConsumerParameter);
 
   return { videoConsumer, audioConsumer };
 }
